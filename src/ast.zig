566b2602ab8b2183638eea1fa2943d10ec9de335
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2023-10-10 14:52:20 +0200

    add support for return statements

--- src/ast.zig
+++ src/ast.zig
@@ -6,23 +6,22 @@ const Literal = struct {
     value: i64,
 
     fn parse(tok: Token) Literal {
-        _ = tok;
-    }
-
-    fn deinit(self: UnaryExpr, allocator: Allocator) void {
-        _ = allocator;
-        _ = self;
+        return Literal{ .value = std.fmt.parseInt(i64, tok.str, 10) catch |e| std.debug.panic("{}\n", .{e}) };
     }
 };
 
 const Identifier = struct {
     name: []const u8,
+
+    fn parse(tok: Token) Identifier {
+        return Identifier{ .name = tok.str };
+    }
 };
 
 const UnaryExpr = struct {
     op: enum {
         log_not,
-        bin_not,
+        bit_not,
     },
     expr: *Expression,
     fn deinit(self: UnaryExpr, allocator: Allocator) void {
@@ -78,23 +77,67 @@ const FunctionCall = struct {
 
 const Expression = union(enum) {
     lit: Literal,
+    ident: Identifier,
     bin: BinaryExpr,
     un: UnaryExpr,
     fn_call: FunctionCall,
 
     fn parse(allocator: Allocator, tokens: *[]const Token) !Expression {
-        _ = allocator;
         var s = tokens.*;
         var res: Expression = undefined;
-        _ = res;
 
-        if (s.len < 2) {
+        if (s[0].is_unary_operator()) {
+            res = Expression{ .un = UnaryExpr{ .op = switch (s[0].tp) {
+                .bit_not => .bit_not,
+                .log_not => .log_not,
+                else => unreachable,
+            }, .expr = try allocator.create(Expression) } };
+            tokens.* = tokens.*[1..];
+            res.un.expr.* = try Expression.parse(allocator, tokens);
+            return res;
+        }
+
+        if (s.len == 0) {
             return error.NotEnoughTokens;
         }
 
-        // s = s[1..];
+        if (s[0].tp == .literal) {
+            res = Expression{ .lit = Literal.parse(s[0]) };
+            s = s[1..];
+        } else if (s[0].tp == .identifier) {
+            res = Expression{ .ident = Identifier.parse(s[0]) };
+            s = s[1..];
+        }
 
-        return Expression{ .lit = Literal{ .value = 0 } };
+        while (s.len > 0 and s[0].is_binary_operator()) {
+            var temp = res;
+            res = Expression{ .bin = BinaryExpr{ .lexpr = try allocator.create(Expression), .op = switch (s[0].tp) {
+                .add => .add,
+                .subtract => .subtract,
+                .multiply => .multiply,
+                .divide => .divide,
+                .bit_or => .bit_or,
+                .bit_and => .bit_and,
+                .bit_xor => .bit_xor,
+                .log_or => .log_or,
+                .log_and => .log_and,
+                .le => .le,
+                .le_eq => .le_eq,
+                .gr => .gr,
+                .gr_eq => .gr_eq,
+                .eq => .eq,
+                .n_eq => .n_eq,
+                else => unreachable,
+            }, .rexpr = try allocator.create(Expression) } };
+            res.bin.lexpr.* = temp;
+            s = s[1..];
+
+            res.bin.rexpr.* = try Expression.parse(allocator, &s);
+        }
+
+        tokens.* = s;
+
+        return res;
     }
 
     fn deinit(self: Expression, allocator: Allocator) void {
@@ -102,6 +145,7 @@ const Expression = union(enum) {
             .bin => |a| a.deinit(allocator),
             .un => |a| a.deinit(allocator),
             .fn_call => |a| a.deinit(allocator),
+            .ident => {},
             .lit => {},
         }
     }
@@ -120,23 +164,27 @@ const If = struct {
     true_branch: Block,
     false_branch: Block,
 
-    fn parse(allocator: Allocator, tokens: *[]const Token) !If {
+    fn parse(allocator: Allocator, tokens: *[]const Token) anyerror!If {
         var s = tokens.*;
         var res: If = undefined;
 
-        if (s.len < 2) {
+        if (s.len < 4) {
             return error.NotEnoughTokens;
         }
 
         s = s[1..];
         res.condition = try Expression.parse(allocator, &s);
 
-        while (s[0].tp != .lbrace) {
-            s = s[1..];
-        }
+        std.debug.print("should be lbrace {}\n", .{s[0].tp});
 
-        const aa = try Block.parse(allocator, &s);
-        _ = aa;
+        res.true_branch = try Block.parse(allocator, &s);
+
+        if (s[0].tp == .else_kw) {
+            s = s[1..];
+            res.false_branch = try Block.parse(allocator, &s);
+        } else {
+            res.false_branch = try Block.empty(allocator);
+        }
 
         tokens.* = s;
 
@@ -145,11 +193,20 @@ const If = struct {
 
     fn deinit(self: If, allocator: Allocator) void {
         self.condition.deinit(allocator);
+        self.true_branch.deinit(allocator);
+        self.false_branch.deinit(allocator);
     }
 };
 
 const Return = struct {
     returned_value: Expression,
+
+    fn parse(allocator: Allocator, tokens: *[]const Token) !Return {
+        tokens.* = tokens.*[1..];
+        const res = Return{ .returned_value = try Expression.parse(allocator, tokens) };
+        tokens.* = tokens.*[1..];
+        return res;
+    }
 
     fn deinit(self: Return, allocator: Allocator) void {
         self.returned_value.deinit(allocator);
@@ -157,9 +214,21 @@ const Return = struct {
 };
 
 const Statement = union(enum) {
-    expr: Expression,
+    expr: Expression, // this is needed to allow print statements
     ret: Return,
     conditional: If,
+
+    fn parse(allocator: Allocator, tokens: *[]const Token) !Statement {
+        return switch (tokens.*[0].tp) {
+            .if_kw => Statement{ .conditional = try If.parse(allocator, tokens) },
+            .return_kw => Statement{ .ret = try Return.parse(allocator, tokens) },
+            else => {
+                const res = Statement{ .expr = try Expression.parse(allocator, tokens) };
+                tokens.* = tokens.*[1..]; // discard semicolon
+                return res;
+            },
+        };
+    }
 
     fn deinit(self: Statement, allocator: Allocator) void {
         switch (self) {
@@ -173,11 +242,17 @@ const Statement = union(enum) {
 const Block = struct {
     statements: *std.ArrayList(Statement),
 
-    fn parse(allocator: Allocator, tokens: *[]const Token) !Block {
-        var s = tokens.*;
+    fn empty(allocator: Allocator) !Block {
         var res: Block = undefined;
         res.statements = try allocator.create(std.ArrayList(Statement));
         res.statements.* = std.ArrayList(Statement).init(allocator);
+        return res;
+    }
+
+    fn parse(allocator: Allocator, tokens: *[]const Token) !Block {
+        var s = tokens.*;
+
+        var res = try Block.empty(allocator);
 
         if (s.len < 2) {
             return error.NotEnoughTokens;
@@ -187,26 +262,14 @@ const Block = struct {
             return error.IncorrectToken;
         }
 
+        // discard left brace
         s = s[1..];
-
-        // while (s[0].tp != .rbrace) {
-        //     if (s[0].tp == .if_kw) {
-        //         try res.statements.append(Statement{ .conditional = try If.parse(allocator, &s) });
-        //         continue;
-        //     }
-
-        //     s = s[1..];
-        // }
-
-
-        // temporary
-        // just discards the entire block
-        var brace_count: usize = 1;
-        while ( brace_count > 0) {
-            brace_count += @intFromBool(s[0].tp == .lbrace);
-            brace_count -= @intFromBool(s[0].tp == .rbrace);
-            s = s[1..];
+        while (s[0].tp != .rbrace) {
+            try res.statements.append(try Statement.parse(allocator, &s));
         }
+
+        // discard right brace
+        s = s[1..];
 
         tokens.* = s;
 
@@ -248,6 +311,7 @@ pub const Function = struct {
             paren_count -= @intFromBool(s[0].tp == .rparen);
             s = s[1..];
         }
+        std.debug.print("parsing function {}\n", .{s[0].tp});
         tokens.* = s;
 
         res.children = try Block.parse(allocator, tokens);
